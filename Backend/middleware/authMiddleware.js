a8b8280181926d781b11c8ddb6c5b90064191d83
Ashish Bedare <ashishbedare9@gmail.com>
2026-02-27 10:57:07 +0530

    Pipelines for Certificate

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -4,10 +4,10 @@ const SECRET_KEY = "supersecretkey";
 function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader)
+  if (!authHeader) {
     return res.status(401).json({ message: "Access Denied" });
+  }
 
-  // Support both raw token and Bearer token
   const token = authHeader.startsWith("Bearer ")
     ? authHeader.split(" ")[1]
     : authHeader;
@@ -16,11 +16,10 @@ function verifyToken(req, res, next) {
     const verified = jwt.verify(token, SECRET_KEY);
     req.user = verified;
     next();
-  } catch {
-    res.status(401).json({ message: "Invalid or Expired Token" });
+  } catch (err) {
+    res.status(400).json({ message: "Invalid or Expired Token" });
   }
 }
-
 function verifyAdmin(req, res, next) {
   if (req.user.role !== "admin")
     return res.status(403).json({ message: "Admin Access Only" });
